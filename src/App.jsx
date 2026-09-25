6f188a29a02f2d5c3dcaeed206b56c85ccd2255b
zannasalamova389-wq <zannasalamova389@gmail.com>
2026-09-25 17:57:00 +0500

    Добавила React-проект и картинки.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Card  from './Card';
 import './App.css';
+import espresso from './images/espresso.jpg';
+import cappuccino from './images/cappuccino.jpg';
+import latte from './images/latte.jpg';
+
 function App() {
   return (
     <div>
       <h1>Наше меню</h1>
       <div className="cards">
-        <Card title="Эспрессо" description="Крепкий и бодрящий" price={150} image="https://via.placeholder.com/200×150" />
-        <Card title="Капучино" description="С нежной молчной пенкой" price={200} image="https://via.placeholder.com/200×150" />
-        <Card title="Латте" description="Мягкий и сливочный" price={200} image="https://via.placeholder.com/200×150" />
+        <Card title="Эспрессо" description="Крепкий и бодрящий" price={150} image={espresso} />
+        <Card title="Капучино" description="С нежной молчной пенкой" price={200} image={cappuccino} />
+        <Card title="Латте" description="Мягкий и сливочный" price={200} image={latte} />
       </div>
     </div>
   );
