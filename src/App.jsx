8d06eaf98ca3d95f13b28a278f50fc014853104e
zannasalamova389-wq <zannasalamova389@gmail.com>
2026-09-26 15:47:25 +0500

    React-проект с eseState и картинками.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import Card  from './Card';
+import { useState } from 'react';
+import Card from './Card';
 import './App.css';
 import espresso from './images/espresso.jpg';
 import cappuccino from './images/cappuccino.jpg';
 import latte from './images/latte.jpg';
-
 function App() {
-  return (
-    <div>
-      <h1>Наше меню</h1>
-      <div className="cards">
-        <Card title="Эспрессо" description="Крепкий и бодрящий" price={150} image={espresso} />
-        <Card title="Капучино" description="С нежной молчной пенкой" price={200} image={cappuccino} />
-        <Card title="Латте" description="Мягкий и сливочный" price={200} image={latte} />
-      </div>
-    </div>
-  );
+const [cartCount, setCartCount] = useState(0);
+function addToCart() {
+setCartCount(cartCount + 1);
+}
+return (
+<div>
+<header className="header">
+<p className="cart-info">🛒 Товаров в корзине: {cartCount}</p>
+</header>
+<div className="cards">
+<Card totle="Эспрессо" description="Крепкий и бодрящий" price={150} image={espresso} onAdd={addToCart} />
+<Card title="Капучино" description="С нежной молочной пенкой" price={200} image={cappuccino} onAdd={addToCart} />
+<Card title="Латте" description="Мягкий и сливочный" price={200} image={latte} onAdd={addToCart} />
+</div>
+</div>
+);
 }
 export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,12 @@
-function Card({ title, description, price, image}) {
+function Card({ title, description, price, image,onAdd}) {
 return (
-   <div className="card">
-     <img src={image} alt={title} />
-     <h3>{title}</h3>
-     <p>{description}</p>
-     <span>{price} ₽</span>
-     <button>В корзину</button>
-   </div>
- );
+<div className="card">
+<img src={image} alt={title} />
+<h3>{title}</h3>
+<p>{description}</p>
+<span>{price} ₽</span>
+<button onClick={onAdd}>В корзину</button>
+</div>
+);
 }
 export default Card;
